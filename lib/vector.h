fbaa1e506fb894cc27f6e4438f7867330d2aee29
Alicia Andries <alicia.andries99>
2023-11-15 14:28:48 +0100

    Action 9: Safe vector implementation with a different interface

--- lib/vector.h
+++ lib/vector.h
@@ -1,20 +1,28 @@
+#include <stdarg.h>
 #include <stdbool.h>
-#include <unistd.h>
+#include <stdint.h>
+#include <stdlib.h>
 
-typedef struct vector vector_t;
+typedef struct Vec_____c_void Vec_____c_void;
 
-vector_t* vector_create(size_t size);
+/**
+ * # Safety
+ * The pointer passed to vec must originate from vector_create.
+ */
+struct Vec_____c_void *vector_create(size_t size);
 
-void vector_add(vector_t* vec, void* element);
+void vector_add(struct Vec_____c_void *vec, void *element);
 
-void vector_remove_at_index(vector_t* vec, size_t index);
+void vector_remove_at_index(struct Vec_____c_void *vec, size_t index);
 
-void* vector_at(vector_t* vec, size_t index);
+void *vector_at(struct Vec_____c_void *vec, uintptr_t index);
 
-void vector_set(vector_t* vec, size_t index, void* new_value);
+void vector_set(struct Vec_____c_void *vec, size_t index, void *new_value);
 
-void* vector_find(vector_t* vec, void* element_to_match, bool (*match_elements)(void*, void*));
+void *vector_find(struct Vec_____c_void *vec,
+                  void *element_to_match,
+                  bool (*match_elements)(void*, void*));
 
-size_t vector_size(vector_t* vec);
+size_t vector_size(struct Vec_____c_void *vec);
 
-void vector_destroy(vector_t* vec);
+void vector_destroy(struct Vec_____c_void *vec);
--- src/vector.rs
+++ src/vector.rs
@@ -1,103 +1,94 @@
 #![allow(non_camel_case_types)]
 pub type size_t = usize;
-#[derive(Copy, Clone)]
-#[repr(C)]
-pub struct vector {
-    pub elements: *mut *mut libc::c_void,
-    pub size: size_t,
-}
-pub type vector_t = vector;
+
+/// # Safety
+/// The pointer passed to vec must originate from vector_create.
 #[no_mangle]
-pub unsafe extern "C" fn vector_set_size(vec: *mut vector_t, number: libc::c_int) {
+pub extern "C" fn vector_create(size: size_t) -> *mut Vec<*mut libc::c_void> {
+    let vector: Vec<*mut libc::c_void> = vec![0 as *mut libc::c_void; size];
+    let boxed_vec: *mut Vec<*mut libc::c_void> = Box::into_raw(Box::new(vector));
+    boxed_vec
+}
+#[no_mangle]
+pub unsafe extern "C" fn vector_add(
+    vec: *mut Vec<*mut libc::c_void>,
+    element: *mut libc::c_void,
+) {
     assert!(!vec.is_null());
-    (*vec).size = number as size_t;
-    (*vec).elements = libc::realloc(
-        (*vec).elements as *mut libc::c_void,
-        (*vec).size * ::core::mem::size_of::<*mut libc::c_void>(),
-    ) as *mut *mut libc::c_void;
-    libc::memset(
-        (*vec).elements as *mut libc::c_void,
-        0,
-        (*vec).size * ::core::mem::size_of::<*mut libc::c_void>(),
-    );
+    let mut boxed_vec = unsafe{Box::from_raw(vec)};
+    boxed_vec.push(element);
+    std::mem::forget(boxed_vec);
 }
 #[no_mangle]
-pub unsafe extern "C" fn vector_create(size: size_t) -> *mut vector_t {
-    let vector: *mut vector_t =
-        libc::calloc(1, ::core::mem::size_of::<vector_t>()) as *mut vector_t;
-    if size != 0 {
-        vector_set_size(vector, size as libc::c_int);
-    }
-    return vector;
-}
-#[no_mangle]
-pub unsafe extern "C" fn vector_add(vec: *mut vector_t, element: *mut libc::c_void) {
+pub unsafe extern "C" fn vector_remove_at_index(
+    vec: *mut Vec<*mut libc::c_void>,
+    index: size_t,
+) {
     assert!(!vec.is_null());
 
-    (*vec).size = (*vec).size + 1;
-    (*vec).size;
-    (*vec).elements = libc::realloc(
-        (*vec).elements as *mut libc::c_void,
-        (*vec).size * ::core::mem::size_of::<*mut libc::c_void>(),
-    ) as *mut *mut libc::c_void;
-    *((*vec).elements).offset(((*vec).size - 1) as isize) = element;
+    let mut boxed_vec = unsafe{Box::from_raw(vec)};
+    boxed_vec.remove(index);
+    std::mem::forget(boxed_vec);
 }
 #[no_mangle]
-pub unsafe extern "C" fn vector_remove_at_index(vec: *mut vector_t, index: size_t) {
+pub unsafe extern "C" fn vector_at(
+    vec: *mut Vec<*mut libc::c_void>,
+    index: usize,
+) -> *mut libc::c_void {
     assert!(!vec.is_null());
-    assert!(index < (*vec).size);
-    if index < (*vec).size - 1 {
-        libc::memmove(
-            ((*vec).elements).offset(index as isize) as *mut libc::c_void,
-            ((*vec).elements).offset(index as isize).offset(1) as *const libc::c_void,
-            ((*vec).size - index - 1) * ::core::mem::size_of::<*mut libc::c_void>(),
-        );
-    }
-    *((*vec).elements).offset(((*vec).size - 1) as isize) = core::ptr::null_mut();
-    (*vec).size = (*vec).size - 1;
-    (*vec).size;
+    let boxed_vec = unsafe{Box::from_raw(vec)};
+    assert!(index < boxed_vec.len());
+    let res = boxed_vec[index];
+    std::mem::forget(boxed_vec);
+    res
 }
 #[no_mangle]
-pub unsafe extern "C" fn vector_at(vec: *mut vector_t, index: size_t) -> *mut libc::c_void {
-    assert!(!vec.is_null());
-    assert!(index < (*vec).size);
-    return *((*vec).elements).offset(index as isize);
-}
-#[no_mangle]
-pub unsafe extern "C" fn vector_set(
-    vec: *mut vector_t,
+pub extern "C" fn vector_set(
+    vec: *mut Vec<*mut libc::c_void>,
     index: size_t,
     new_value: *mut libc::c_void,
 ) {
     assert!(!vec.is_null());
-    assert!(index < (*vec).size);
-    *((*vec).elements).offset(index as isize) = new_value;
+
+    let mut boxed_vec = unsafe{Box::from_raw(vec)};
+    boxed_vec[index] = new_value;
+    std::mem::forget(boxed_vec);
 }
+
 #[no_mangle]
-pub unsafe extern "C" fn vector_find(
-    vec: *mut vector_t,
+pub extern "C" fn vector_find(
+    vec: *mut Vec<*mut libc::c_void>,
     element_to_match: *mut libc::c_void,
-    match_elements: Option<unsafe extern "C" fn(*mut libc::c_void, *mut libc::c_void) -> bool>,
+    match_elements: Option::<
+        unsafe extern "C" fn(*mut libc::c_void, *mut libc::c_void) -> bool,
+    >,
 ) -> *mut libc::c_void {
     assert!(!vec.is_null());
-    let mut i: size_t = 0;
-    while i < (*vec).size {
-        let element: *mut libc::c_void = vector_at(vec, i);
-        if match_elements.expect("non-null function pointer")(element, element_to_match) {
-            return element;
-        }
-        i = i + 1;
+
+    let boxed_vec = unsafe{Box::from_raw(vec)};
+    let res = boxed_vec.iter().rfind(|&&element| unsafe{match_elements.expect("non-null function pointer")(element, element_to_match)} == true);
+    let real_res = match res {
+        Some(s) => *s,
+        None => 0 as *mut libc::c_void,
+    };
+    std::mem::forget(boxed_vec);
+    real_res
+}
+
+#[no_mangle]
+pub extern "C" fn vector_size(vec: *mut Vec<*mut libc::c_void>) -> size_t {
+    assert!(!vec.is_null());
+
+    let boxed_vec = unsafe{Box::from_raw(vec)};
+    let len = boxed_vec.len();
+    std::mem::forget(boxed_vec);
+    len
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn vector_destroy(vec: *mut Vec<*mut libc::c_void>) {
+    assert!(!vec.is_null());
+    unsafe{
+        drop(Box::from_raw(vec));
     }
-    return core::ptr::null_mut();
-}
-#[no_mangle]
-pub unsafe extern "C" fn vector_size(vec: *mut vector_t) -> size_t {
-    assert!(!vec.is_null());
-    return (*vec).size;
-}
-#[no_mangle]
-pub unsafe extern "C" fn vector_destroy(vec: *mut vector_t) {
-    assert!(!vec.is_null());
-    libc::free((*vec).elements as *mut libc::c_void);
-    libc::free(vec as *mut libc::c_void);
 }
